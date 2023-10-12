6e9a90dfbf700ad21d99b450252bf6b59c9ac353
danielborda27 <dansanbor@gmail.com>
2023-10-11 20:52:17 -0500

    feat(back): configure service to get commits lists from github

--- backend/show-git-commits/src/github/github.service.ts
+++ backend/show-git-commits/src/github/github.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { IResponseCommit } from './commit.interface';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+  private readonly apiUrl = 'https://api.github.com';
+
+  async getCommits(owner: string, repo: string): Promise<IResponseCommit[]> {
+    return await axios.get(`${this.apiUrl}/repos/${owner}/${repo}/commits`);
+  }
+}
